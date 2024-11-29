5ea95e47708d7df0335ef5cf002fc02f13c660cc
AnotherDvil <miguel.angel.lopez.gonzales.823@gmail.com>
2024-11-28 21:19:09 -0600

    Cambios MAHL

--- src/pages/client/PropuestasPage.tsx
+++ src/pages/client/PropuestasPage.tsx
@@ -5,6 +5,9 @@ import GradeIcon from '@mui/icons-material/Grade';
 import ShareIcon from '@mui/icons-material/Share';
 import BackHandIcon from '@mui/icons-material/BackHand';
 import AddIcon from '@mui/icons-material/Add';
+import AutoModeSharpIcon from '@mui/icons-material/AutoModeSharp'; // Importa el ícono
+import ShareModal from '../../components/ShareModal';// modal de compartir 
+import noImage from '../../assets/NoImagen.png';
 import { useMediaQuery } from '@mui/material';
 
 interface ServiceDetail {
@@ -40,44 +43,117 @@ const ProposalDetail: React.FC<ProposalDetailProps> = ({ darkMode }) => {
   const [deliberationEndDate, setDeliberationEndDate] = useState('');
   const isSmallScreen = useMediaQuery('(max-width:600px)');
   const navigate = useNavigate();
+  const [isShareModalOpen, setIsShareModalOpen] = useState(false);//Modal
+  const token = localStorage.getItem('token');
 
+
+  // Llamar a la API cada 5 segundos
   useEffect(() => {
-    let isMounted = true;
-    if (id) {
-      fetch(`/services/${id}`)
-        .then((res) => res.json())
-        .then((data) => {
-          if (isMounted && data.length > 0) {
-            setService(data[0]);
+    const fetchServiceAndProposals = async () => {
+      try {
+        if (id) {
+          const serviceResponse = await fetch(`/services/${id}`);
+          const serviceData = await serviceResponse.json();
+          if (serviceData.length > 0) {
+            setService(serviceData[0]);
           }
-        })
-        .catch((error) => console.error('Error al obtener los detalles del servicio:', error.message));
+        }
+      } catch (error) {
+        if (error instanceof Error) {
+          console.error('Error al obtener los detalles del servicio:', error.message);
+        } else {
+          console.error('Error desconocido al obtener los detalles del servicio:', error);
+        }
+      }
 
-      fetch(`/proposals/${id}`)
-        .then((res) => res.json())
-        .then((data) => isMounted && setProposals(data))
-        .catch((error) => console.error('Error al obtener propuestas:', error.message));
-    }
-    return () => { isMounted = false; };
+      try {
+        if (id) {
+          const proposalsResponse = await fetch(`/proposals/${id}`);
+          const proposalsData = await proposalsResponse.json();
+          setProposals(proposalsData);
+        }
+      } catch (error) {
+        if (error instanceof Error) {
+          console.error('Error al obtener propuestas:', error.message);
+        } else {
+          console.error('Error desconocido al obtener propuestas:', error);
+        }
+      }
+    };
+
+    fetchServiceAndProposals(); // Llamada inicial
+    const intervalId = setInterval(fetchServiceAndProposals, 5000); // Llamada cada 5 segundos
+
+    // Limpiar el intervalo al desmontarse
+    return () => clearInterval(intervalId);
   }, [id]);
 
   if (!service) {
     return <p>Cargando detalles del servicio...</p>;
   }
 
-  const handleClickOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-  const handleSave = () => {
-    const newProposalData = {
-      name: proposalName,
-      description: proposalDescription,
-      debateEndDate,
-      deliberationEndDate,
-    };
-    console.log('Guardando propuesta:', newProposalData); // Log para ver los datos enviados
 
-    handleClose();
+
+  const handleClickOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
   };
+    
+  const handleSave = async () => {
+    // Verifica que los campos obligatorios no estén vacíos
+    if (!proposalName || !proposalDescription || !debateEndDate || !deliberationEndDate) {
+      alert('Todos los campos son obligatorios. Por favor, completa toda la información.');
+      return; // Detiene el envío de la propuesta si los campos están vacíos
+    }
+  
+    const serviceId = id ? parseInt(id) : null; // Obtén el service_id desde la URL
+  
+    if (!serviceId) {
+      console.error("ID del servicio no encontrado.");
+      return;
+    }
+  
+    const newProposalData = {
+      params: {
+        name: proposalName,
+        written_by: token || "", // Token obtenido del localStorage
+        description: proposalDescription,
+        service_id: serviceId,
+        debateEndDate,
+        deliberationEndDate,
+      },
+    };
+  
+    try {
+      const response = await fetch("/proposals/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newProposalData),
+      });
+  
+      if (response.ok) {
+        const data = await response.json();
+        console.log("Propuesta creada exitosamente:", data);
+        // Aquí puedes actualizar la lista de propuestas o mostrar un mensaje de éxito
+      } else {
+        console.error("Error al crear la propuesta:", response.status, response.statusText);
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error("Error al realizar la solicitud:", error.message);
+      } else {
+        console.error("Error al realizar la solicitud:", error);
+      }
+  
+    } finally {
+      handleClose(); // Siempre cierra el modal
+    }
+  };
+  
+
+  
 
   const handleNovedadesClick = () => {
     navigate(`/services/${id}`); // Cambia a una ruta relativa
@@ -98,7 +174,7 @@ const ProposalDetail: React.FC<ProposalDetailProps> = ({ darkMode }) => {
               <CardMedia
                 component="img"
                 height={isSmallScreen ? '200' : '300'}
-                image={service.image ? `data:image/jpg;base64,${atob(service.image)}` : "https://w.wallhaven.cc/full/o5/wallhaven-o5xmv9.jpg"}
+                image={service.image ? `data:image/jpg;base64,${atob(service.image)}` :noImage}
                 alt={service.name}
                 className='image-service'
                 style={{ filter: 'brightness(0.7)' }}
@@ -115,33 +191,46 @@ const ProposalDetail: React.FC<ProposalDetailProps> = ({ darkMode }) => {
               >
                 {service.name}
               </Typography>
-              <Typography
-                variant="body2"
-                color="text.secondary"
-                style={{
-                  position: 'absolute',
-                  bottom: '10px',
-                  left: '10px',
-                  color: 'white',
-                  padding: '5px',
-                }}
-              >
-                <Rating name="read-only" value={service.qualification} readOnly />
-              </Typography>
+              <div
+                  style={{
+                    position: 'absolute',
+                    bottom: '10px',
+                    left: '10px',
+                    display: 'flex',
+                    alignItems: 'center',
+                    color: 'white', // Asegura que el texto y estrellas sean visibles
+                    padding: '5px',
+                  }}
+                >
+                  <Rating
+                    name="read-only"
+                    value={service.qualification || 0}
+                    precision={0.5}
+                    readOnly
+                  />
+                  <Typography
+                    variant="body2"
+                    color="text.secondary"
+                    style={{ marginLeft: '10px' ,}}
+                  >
+                    {service.qualification ? service.qualification.toFixed(1) : '0.0'}
+                  </Typography>
+                </div>
+
             </Box>
 
             <CardContent>
-              <Typography variant="body2" color="text.secondary" align='left' paddingBottom={'5px'}>
-                Descripción: {service.description || "Sin descripción disponible"}
+              <Typography variant="body2" color="text.secondary" align='left' paddingBottom={'5px'} style={{ paddingTop: '15px', paddingBottom: '15px' }}>
+                {service.description || "Sin descripción disponible"}
               </Typography>
             </CardContent>
 
             <CardContent>
               <Stack spacing={1} direction="row">
-                <Button variant="contained" startIcon={<GradeIcon />} style={{ fontSize: isSmallScreen ? '0.7rem' : '0.9rem', padding: isSmallScreen ? '4px 8px' : '6px 12px' }}  onClick={handleNovedadesClick}>Novedades</Button>
+                <Button variant="contained" startIcon={<AutoModeSharpIcon/>} style={{ fontSize: isSmallScreen ? '0.7rem' : '0.9rem', padding: isSmallScreen ? '4px 8px' : '6px 12px' }}  onClick={handleNovedadesClick}>Novedades</Button>
                 <Button variant="contained" startIcon={<GradeIcon />} style={{ fontSize: isSmallScreen ? '0.7rem' : '0.9rem', padding: isSmallScreen ? '4px 4px' : '6px 12px' }}onClick={() => navigate(`/services/${id}/reviews`)}>Reseñas</Button>
                 <Button variant="contained" startIcon={<BackHandIcon />} style={{ fontSize: isSmallScreen ? '0.7rem' : '0.9rem', padding: isSmallScreen ? '4px 8px' : '6px 12px' }}>Propuestas</Button>
-                <Button variant="outlined" startIcon={<ShareIcon />} style={{ fontSize: isSmallScreen ? '0.7rem' : '0.9rem', padding: isSmallScreen ? '4px 8px' : '6px 12px' }}>Compartir</Button>
+                <Button variant="outlined" startIcon={<ShareIcon />} onClick={() => setIsShareModalOpen(true)}> Compartir</Button>
                 <Button variant="contained" startIcon={<AddIcon />} style={{ fontSize: isSmallScreen ? '0.7rem' : '0.9rem', padding: isSmallScreen ? '4px 8px' : '6px 12px' }}>Seguir</Button>
               </Stack>
 
@@ -203,22 +292,34 @@ const ProposalDetail: React.FC<ProposalDetailProps> = ({ darkMode }) => {
                         <TableCell><Typography variant="subtitle2">Título</Typography></TableCell>
                         <TableCell><Typography variant="subtitle2">Estado</Typography></TableCell>
                         <TableCell><Typography variant="subtitle2">Creado por</Typography></TableCell>
-                        <TableCell><Typography variant="subtitle2">Última actualización</Typography></TableCell>
+                        <TableCell><Typography variant="subtitle2">Fecha de cierre</Typography></TableCell>
                       </TableRow>
                     </TableHead>
                     <TableBody>
-                      {proposals.map((proposal) => (
-                        <TableRow key={proposal.id} onClick={() => handleRowClick(proposal.id)} style={{ cursor: 'pointer' }}>
-                          <TableCell>{proposal.name || "Sin título"}</TableCell>
-                          <TableCell><Chip label={proposal.status || "No disponible"} style={{ backgroundColor: '#fdd835', color: '#000' }} /></TableCell>
-                          <TableCell>
-                            <Avatar style={{ width: 24, height: 24, marginRight: 8 }} />
-                            {proposal.written_by || "Desconocido"}
-                          </TableCell>
-                          <TableCell>{proposal.create_date || "Sin fecha"}</TableCell>
-                        </TableRow>
-                      ))}
-                    </TableBody>
+                {proposals.map((proposal) => (
+                  <TableRow
+                    key={proposal.id}
+                    onClick={() => handleRowClick(proposal.id)}
+                    style={{ cursor: 'pointer' }}
+                  >
+                    <TableCell>{proposal.name || "Sin título"}</TableCell>
+                    <TableCell>
+                      <Chip
+                        label={proposal.status || "No disponible"}
+                        style={{ backgroundColor: '#fdd835', color: '#000' }}
+                      />
+                    </TableCell>
+                    <TableCell>
+                      <div style={{ display: 'flex', alignItems: 'center' }}>
+                        <Avatar style={{ width: 24, height: 24, marginRight: 8 }} />
+                        <span>{proposal.written_by || "Desconocido"}</span>
+                      </div>
+                    </TableCell>
+                    <TableCell>{proposal.close_date || "Sin fecha"}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+
                   </Table>
                 </TableContainer>
               ) : (
@@ -232,6 +333,8 @@ const ProposalDetail: React.FC<ProposalDetailProps> = ({ darkMode }) => {
           <Box display="flex" justifyContent="flex-end" alignItems="flex-end" p={0} style={{ overflow: 'hidden' }}>
             <Button variant="contained" color="primary" onClick={handleClickOpen}>Crear Propuesta</Button>
           </Box>
+
+          <ShareModal open={isShareModalOpen} onClose={() => setIsShareModalOpen(false)} />
         </div>
       </div>
     </div>
