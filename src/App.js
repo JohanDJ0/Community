498ae802af2c91f89c65fe2ceb9a361342d3c8d7
Anorak42 <127681664+Anora42@users.noreply.github.com>
2024-12-07 00:13:08 -0600

    App js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import HomePage from './pages/client/HomePage';
 import SeguidosPage from './pages/client/SeguidosPage';
@@ -22,80 +22,102 @@ import ProposalDetail from 'pages/client/ProposalDetail';
 import MyProposals from 'pages/owner/MyProposals';
 import RecompensasCanjedas from 'pages/owner/RecompensasCanjeadas';
 
+
 const Layout = ({ children, toggleDarkMode }) => {
-  const location = useLocation();
+ const location = useLocation();
+
 
   // Bloquear el scroll del body en todas las vistas
   useEffect(() => {
-    document.body.style.overflow = 'hidden'; // Desactiva el scroll en todas las páginas
+   document.body.style.overflow = 'hidden'; // Desactiva el scroll en todas las páginas
 
-    // Limpiar el efecto al desmontar el componente
-    return () => {
-      document.body.style.overflow = 'auto'; // O restablecer si es necesario
-    };
-  }, [location.pathname]);
 
-  return (
-    <>
-      {location.pathname !== '/' && location.pathname !== '/Login' && location.pathname !== '/VistaRol' && (
-        <Header toggleDarkMode={toggleDarkMode} />
-      )}
-      <div className="App">
-        <div className="content">{children}</div>
-      </div>
-    </>
-  );
+   // Limpiar el efecto al desmontar el componente
+   return () => {
+     document.body.style.overflow = 'auto'; // O restablecer si es necesario
+   };
+ }, [location.pathname]);
+
+
+ return (
+   <>
+     {location.pathname !== '/' && location.pathname !== '/Login' && location.pathname !== '/VistaRol' && (
+       <Header toggleDarkMode={toggleDarkMode} />
+     )}
+     <div className="App">
+       <div className="content">{children}</div>
+     </div>
+   </>
+ );
 };
+
 
 const App = () => {
-  const [darkMode, setDarkMode] = useState(false);
+ const [darkMode, setDarkMode] = useState(false);
 
-  const theme = createTheme({
-    palette: {
-      mode: darkMode ? 'dark' : 'light',
-    },
-  });
 
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-  };
+ const theme = createTheme({
+   palette: {
+     mode: darkMode ? 'dark' : 'light',
+   },
+ });
 
-  // Asegúrate de establecer el data-theme en el body
-  useEffect(() => {
-    document.body.setAttribute('data-theme', darkMode ? 'dark' : 'light');
-  }, [darkMode]);
 
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Router>
-        <Layout toggleDarkMode={toggleDarkMode}>
-          <Routes>
-            <Route path="/" element={<HomePage darkMode={darkMode} />} />
-            <Route path="/Seguidos" element={<SeguidosPage darkMode={darkMode} />} />
-            <Route path="/Rewards" element={<RewardsPage darkMode={darkMode} />} />
-            <Route path="/JoinBusiness" element={<JoinBusinessPage darkMode={darkMode} />} />
-            <Route path="/Profile" element={<ProfilePage darkMode={darkMode} />} />
-            <Route path="/Login" element={<Login />} />
-            <Route path="/Services" element={<Services darkMode={darkMode} />} />
-            <Route path="/services/:id" element={<ServiceDetail darkMode={darkMode} />} />
-            <Route path="/VistaRol" element={<VistaRol darkMode={darkMode} />} />
-            <Route path="/MyService" element={<MyServicePage darkMode={darkMode} />} />
-            <Route path="/services/:id/reviews" element={<ServiceReviewsPage darkMode={darkMode} />} />
+ const toggleDarkMode = () => {
+   setDarkMode(!darkMode);
+ };
 
-            <Route path="/MyService/:id/reviews" element={<MyReviews darkMode={darkMode} />} />
-            <Route path="/MyProposals/:id" element={<MyProposals darkMode={darkMode} />} />
 
-            <Route path='/Empleados' element={<EmpleadosPage darkMode={darkMode} />} />
-            <Route path='/MyRewards' element={<MyRewardsPage darkMode={darkMode} />} />
-            <Route path="/proposal/:id" element={<PropuestasPage darkMode={darkMode} />} />
-            <Route path="/ProposalDetail/:serviceId/:proposalId/:serviceName" element={<ProposalDetail darkMode={darkMode} />} />
-            <Route path='/rewardsRedeemed' element={<RecompensasCanjedas darkMode={darkMode} />} />
-          </Routes>
-        </Layout>
-      </Router>
-    </ThemeProvider>
-  );
+ // Asegúrate de establecer el data-theme en el body
+ useEffect(() => {
+   document.body.setAttribute('data-theme', darkMode ? 'dark' : 'light');
+ }, [darkMode]);
+
+
+ return (
+   <Suspense fallback={<div>Cargando...</div>}>
+     <ThemeProvider theme={theme}>
+       <CssBaseline />
+       <Router>
+         <Layout toggleDarkMode={toggleDarkMode}>
+           <Routes>
+             <Route path="/" element={<HomePage darkMode={darkMode}  />} />
+             <Route path="/Seguidos" element={<SeguidosPage darkMode={darkMode} />} />
+             <Route path="/Rewards" element={<RewardsPage darkMode={darkMode} />} />
+             <Route path="/JoinBusiness" element={<JoinBusinessPage darkMode={darkMode} />} />
+             <Route path="/Profile" element={<ProfilePage darkMode={darkMode} />} />
+             <Route path="/Login" element={<Login />} />
+             <Route path="/Services" element={<Services darkMode={darkMode} />} />
+             <Route path="/services/:id" element={<ServiceDetail darkMode={darkMode} />} />
+             <Route path="/VistaRol" element={<VistaRol darkMode={darkMode} />} />
+             <Route path="/MyService" element={<MyServicePage darkMode={darkMode}/>}/>
+             <Route path="/services/:id/reviews" element={<ServiceReviewsPage darkMode={darkMode} />} />
+
+
+             <Route path="/MyService/:id/reviews" element={<MyReviews darkMode={darkMode} />} />
+             <Route path="/MyProposals/:id" element={<MyProposals darkMode={darkMode} />} />
+
+
+             <Route path='/Empleados' element={<EmpleadosPage darkMode={darkMode}/>}/>
+             <Route path='/MyRewards' element={<MyRewardsPage darkMode={darkMode}/>}/>
+             <Route path="/proposal/:id" element={<PropuestasPage darkMode={darkMode}/>} />
+             <Route path="/ProposalDetail/:serviceId/:proposalId/:serviceName" element={<ProposalDetail darkMode={darkMode} />} />
+             <Route path='/rewardsRedeemed' element={<RecompensasCanjedas darkMode={darkMode}/>}/>
+           </Routes>
+         </Layout>
+       </Router>
+     </ThemeProvider>
+   </Suspense>
+ );
 };
 
+
 export default App;
+
+
+
+
+
+
+
+
